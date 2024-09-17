adfebf7d6a2f7d9abc6647a6e88d75c093a76963
Pau Kuntong <paudcin@gmail.com>
2024-09-16 21:54:10 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-This repository follows along with the Webpack chapter in TripleTen's Software Engineering bootcamp. Each commit corresponds to a lesson in the chapter. If you are following along locally, use this repository to check your work.
+# Webpack Demo
 
+This repository follows along with the Webpack chapter in TripleTen's Software
+Engineering bootcamp. Each commit corresponds to a lesson in the chapter. If
+you are following along locally, use this repository to check your work.
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
@@ -21,4 +24,38 @@ module.exports = {
     liveReload: true,
     hot: false,
   },
+  target: ["web", "es5"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
 };
